0770efa5e4b735217139fa6c1495d490de631855
tushar-glitch <tusharc20001@gmail.com>
2024-12-17 19:06:01 +0530

    Operator overloading

---
+++ Random/OperatorOverloading.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Complex{
+    int x, y;
+    
+    public:
+        Complex(){
+            x = 0, y = 0;
+        }
+        Complex(int i, int j){
+            x = i, y = j;
+        }
+        Complex operator+(Complex op){
+            Complex temp;
+            temp.x = x + op.x;
+            temp.y = y + op.y;
+            return temp;
+        }
+        void show(){
+            cout<<x<<" "<<y<<endl;
+        }
+};
+
+int main()
+{
+    Complex a(1,3), b(2, 4);
+    Complex c = a+b;
+    c.show();
+    return 0;
+}
