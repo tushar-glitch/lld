029f0d10b0e29cb3e62c22a4656abedf518ee243
tushar-glitch <tusharc20001@gmail.com>
2025-01-07 22:15:44 +0530

    inheritance

--- Random/Inheritance/basic.cpp
+++ Random/Inheritance/basic.cpp
@@ -31,10 +31,10 @@ std::ostream& operator<<(std::ostream& out, const Banana& obj){
 
 int main()
 {
-    const Apple a{ "Red delicious", "red", 4.2 };
+  const Apple a{ "Red delicious", "red", 4.2 };
 	std::cout << a << '\n';
 
 	const Banana b{ "Cavendish", "yellow" };
 	std::cout << b << '\n';
-    return 0;
+  return 0;
 }
